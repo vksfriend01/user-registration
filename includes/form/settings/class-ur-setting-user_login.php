83833f85bff03d397ed1423ad6a42c46c0a0aa3d
Prabesh21 <prabeshupreti21@gmail.com>
2020-09-17 08:39:18 +0545

    Enhance- Option added in field option of username in advance setting

--- includes/form/settings/class-ur-setting-user_login.php
+++ includes/form/settings/class-ur-setting-user_login.php
@@ -41,6 +41,20 @@ class UR_Setting_User_login extends UR_Field_Settings {
 				'placeholder' => __( 'Custom Class', 'user-registration' ),
 				'tip'         => __( 'Class name to embed in this field.', 'user-registration' ),
 			),
+			'special_characters'    => array(
+				'type'     => 'select',
+				'data-id'  => $this->field_id . '_special_characters',
+				'label'    => __( 'Allow Special Characters in Username', 'user-registration' ),
+				'name'     => $this->field_id . '[special_characters]',
+				'class'    => $this->default_class . 'ur-settings-special-characters',
+				'default'  => 'false',
+				'required' => false,
+				'options'  => array(
+					'true'  => 'Yes',
+					'false' => 'No',
+				),
+				'tip'      => __( 'Select yes to allow special characters in username.', 'user-registration' ),
+			),
 		);
 
 		$this->render_html( $fields );
